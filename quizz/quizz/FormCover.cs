c38097db8b564ed48b853e02cabf540f005fe087
cosminaMR <61265981+cosminaMR@users.noreply.github.com>
2020-05-02 12:03:45 +0300

    menu
    Co-Authored-By: cosminamr <cosminamr@users.noreply.github.com>

---
+++ quizz/quizz/FormCover.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quizz
+{
+    public partial class FormCover : Form
+    {
+        public FormCover()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void FormMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormMenu newFormM = new FormMenu();
+            newFormM.Show();
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ quizz/quizz/FormMenu.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quizz
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMenu_Load(object sender, EventArgs e)
+        {
+            panel1.BackColor = Color.FromArgb(100, 0, 0, 0);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormStart newFormS = new FormStart();
+            newFormS.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FormStart newFormS = new FormStart();
+            newFormS.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FormStart newFormS = new FormStart();
+            newFormS.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FormStart newFormS = new FormStart();
+            newFormS.Show();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
